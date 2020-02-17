a3e5d0d3d6dbaf4fec2db10f1706c00c343de9e2
bdavis171 <bdavis171@gmail.com>
2020-02-17 13:56:58 -0600

    Work finished

--- index.js
+++ index.js
@@ -32,15 +32,54 @@
 // }
 
 //Problem 4
-let secretWord = "secret";
-let userWord1 = prompt("Enter a word");
-let userWord2 = prompt("Enter another word");
+// let secretWord = "secret";
+// let userWord1 = prompt("Enter a word");
+// let userWord2 = prompt("Enter another word");
 
-if (userWord1 == userWord2 && userWord1 == secretWord && userWord2 == secretWord) {
-    alert("You got in...but we don't know how");
 
-    
+// if (userWord1 == userWord2 && userWord1 != secretWord && userWord2 != secretWord) {
+//     alert("You got in...but only because your words matched");
+// }
+// else if ((userWord1 == secretWord || userWord2 == secretWord) && (userWord1 != secretWord || userWord2 != secretWord)) {
+//     alert("You got in...but only because one of your words matched the secret word");
+// }
+// else if (userWord1 == userWord2 && userWord1 == secretWord && userWord2 == secretWord) {
+//     alert("You got in!");
+// }
+// else {
+//     alert("Locked Out");
+// }
+
+//Challenge
+let day = prompt("Enter a day of the week");
+
+if (day == "Monday" || day == "monday") {
+    alert("Monday is 'lunes' in Spanish");
 }
- if (userWord1 == userWord2 && userWord1 != secretWord && userWord2 != secretWord) {
-    alert("You got in...but only because your words matched");
+
+else if (day == "Tuesday" || day == "tuesday"){
+    alert("Tuesday is 'martes' in Spanish");
+}
+
+else if (day == "Wednesday" || day == "wednesday"){
+    alert("Wednesday is 'miercoles' in Spanish");
+}
+
+else if (day == "Thursday" || day == "thursday"){
+    alert("Thursday is 'jueves' in Spanish");
+}
+
+else if (day == "Friday" || day == "friday"){
+    alert("Friday is 'viernes' in Spanish");
+}
+
+else if (day == "Saturday" || day == "saturday"){
+    alert("Saturday is 'sabado' in Spanish");
+}
+else if (day == "Sunday" || day == "sunday"){
+    alert("Sunday is 'domingo' in Spanish");
+}
+
+else {
+    alert("That is not a day of the week in English");
 }
